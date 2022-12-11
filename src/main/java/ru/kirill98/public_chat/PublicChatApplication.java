916c86c5fa147e5ce7ee1a06b9726da8b8cbcabb
Kirill <k.lubarskih@mail.ru>
2022-12-12 03:35:50 +0400

    Initial commit

---
+++ src/main/java/ru/kirill98/public_chat/PublicChatApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill98.public_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicChatApplication.class, args);
+	}
+
+}
